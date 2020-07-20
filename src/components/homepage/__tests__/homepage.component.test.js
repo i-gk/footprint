848b67eba9b7d6f68499ff5055154d25564b5c71
Gayan Kumarage <gayan_ac@hotmail.com>
2020-07-20 16:07:53 +0530

    unit tests for homepage component changes

--- src/components/homepage/__tests__/homepage.component.test.js
+++ src/components/homepage/__tests__/homepage.component.test.js
@@ -5,6 +5,7 @@ import thunk from "redux-thunk";
 
 import Homepage from "../homepage.component";
 import { Provider } from "react-redux";
+import * as MemoriesActions from "../../../redux/actions/memories.actions";
 
 afterEach(cleanup);
 
@@ -13,6 +14,10 @@ const ReduxProvider = ({ children, store }) => (
 );
 const middlewares = [thunk];
 const store = configureMockStore(middlewares);
+
+beforeEach(() => {
+ jest.clearAllMocks();
+});
 
 describe("Homepage Component", () => {
  it("Should render without crashing", () => {
@@ -33,5 +38,25 @@ describe("Homepage Component", () => {
    </ReduxProvider>
   );
   expect(container.querySelector("#footprint-appheader")).not.toBeNull();
+ });
+
+ it("should fetch latest memories upon rendering", () => {
+  MemoriesActions.getMemories = jest.fn();
+  MemoriesActions.getMemories.mockImplementation(() => {
+   return { type: "FETCH_MEMORIES_SUCCESS", payload: [] };
+  });
+
+  const mockStore = store({
+   memories: {
+    previews: [],
+   },
+  });
+  const { container, debug } = render(
+   <ReduxProvider store={mockStore}>
+    <Homepage />
+   </ReduxProvider>
+  );
+
+  expect(MemoriesActions.getMemories).toHaveBeenCalledTimes(1);
  });
 });
